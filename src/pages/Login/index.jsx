ba37fe4e832d6c9abfa6c503cdd85d2e5fbe41c3
Renan Nascimento <atomsfrontend@gmail.com>
2025-05-05 15:18:47 -0300

    feat: created component of Login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,9 @@
+import { Title } from './styled';
+
 export default function Login() {
   return (
     <div className="card">
-      <h1>Login</h1>
+      <Title $isRed={true}>Login</Title>
       <form action="http://localhost:3000/login" method="POST">
         <input type="text" name="username" placeholder="Username" required />
         <input
