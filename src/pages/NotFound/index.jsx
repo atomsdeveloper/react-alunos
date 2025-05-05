8911261d1e1614319bc6d6dbf771d2ca69606392
Renan Nascimento <atomsfrontend@gmail.com>
2025-05-05 16:14:47 -0300

    feat: created component of NotFound

---
+++ src/pages/NotFound/index.jsx
@@ -0,0 +1,14 @@
+// Styled Components
+import {} from './styled';
+
+// Conatiner Global
+import { Container } from '../../styles/GlobalStyles';
+
+export default function NotFound() {
+  return (
+    <Container>
+      <h1>404 - Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </Container>
+  );
+}
