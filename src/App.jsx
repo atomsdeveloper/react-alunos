0bb43cb760204cd5ecf9dcc1c30a582310db6d5f
Renan Nascimento <atomsfrontend@gmail.com>
2025-05-05 15:35:14 -0300

    feat: use component Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,18 @@
-import { useState, useEffect } from 'react';
-
 // Global Styles
-import './styles/GlobalStyles.js';
+import { GlobalStyles } from './styles/GlobalStyles.js';
+
+// Components
+import Header from './components/Header';
 
 // Pages
 import Login from './pages/Login';
 
 function App() {
-  const [data, setData] = useState(0);
-
-  // Fetch data from the server when the component mounts
-  useEffect(() => {
-    fetch('http://localhost:3000/', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-      credentials: 'include',
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setData(data);
-      })
-      .catch((error) => {
-        console.error('There was a problem with the fetch operation:', error);
-      });
-
-    return () => {};
-  }, []);
-
   return (
     <>
+      <Header />
       <Login />
+      <GlobalStyles />
     </>
   );
 }
