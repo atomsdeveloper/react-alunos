396fe935626aea06b99a18456c2eb338c2e322fa
Renan Nascimento <atomsfrontend@gmail.com>
2025-05-08 13:45:08 -0300

    feat: created new routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -6,21 +6,48 @@ import PrivateRoute from './PrivateRoute.jsx';
 
 // Pages
 import Login from '../pages/Login';
+import Register from '../pages/Register';
 import Students from '../pages/Students';
+import Student from '../pages/Student';
+import Photos from '../pages/Photos';
 import NotFound from '../pages/NotFound';
 
 export default function RoutesClient() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={<Students />} />
+
       <Route
-        path="/estudantes"
+        path="/aluno/:id/edit"
         element={
           <PrivateRoute isClosed>
-            <Students />
+            <Student />
           </PrivateRoute>
         }
       />
+
+      <Route
+        path="/aluno"
+        element={
+          <PrivateRoute isClosed>
+            <Student />
+          </PrivateRoute>
+        }
+      />
+
+      <Route
+        path="/fotos/:id"
+        element={
+          <PrivateRoute isClosed>
+            <Photos />
+          </PrivateRoute>
+        }
+      />
+
+      <Route path="/login" element={<Login />} />
+
+      <Route path="/register" element={<Register />} />
+
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
