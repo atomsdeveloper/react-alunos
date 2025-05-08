74d03bbf9f59ffdefeb804f80828ddc9f44227fa
Renan Nascimento <atomsfrontend@gmail.com>
2025-05-08 20:56:52 -0300

    feat: created component login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,10 +1,78 @@
 // Styled Component
 import { Container } from '../../styles/GlobalStyles';
 
+// React Router
+import { useNavigate } from 'react-router-dom';
+
+// Validator
+import isEmail from 'validator/lib/isEmail';
+
+// Tostify
+import { toast } from 'react-toastify';
+
+// Redux
+import { useDispatch } from 'react-redux';
+
+// Auth
+import * as Actions from '../../store/modules/auth/actions';
+
 export default function Login() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [name, setName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    let formErrors = false;
+
+    if (!isEmail(email)) {
+      formErrors = true;
+      toast.warn('E-mail não é válido.');
+    }
+
+    if (pass.length < 1 || pass.length > 100) {
+      formErrors = true;
+      toast.warn('Campo senha precisa ter entre 12 e 100 caracteres.');
+    }
+
+    if (formErrors) return;
+
+    dispatch(Actions.ButtonLoginClickRequest({ email, password }));
+  }
+
   return (
     <Container>
       <h1>Login</h1>
+
+      <Form onSubmit={handleSubmit}>
+        <LabelContainer>
+          <label htmlFor="name">Name:</label>
+          <input
+            id="name"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Digite seu nome..."
+            aria-label="Campo para inserir o seu nome."
+            autoComplete="name"
+          />
+        </LabelContainer>
+        <LabelContainer>
+          <label htmlFor="email">E-mail:</label>
+          <input
+            id="email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Digite seu e-mail..."
+            aria-label="Campo para inserir o seu e-mail de login."
+            autoComplete="email"
+          />
+        </LabelContainer>
+        <button type="submit">Entrar</button>
+      </Form>
     </Container>
   );
 }
--- src/pages/Login/styled.js
+++ src/pages/Login/styled.js
@@ -1,7 +1,31 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
-  color: ${(props) => (props.$isRed ? 'red' : 'black')};
-  font-size: 4rem;
-  margin-bottom: 1rem;
+// Config Colors
+import * as colors from '../../config/colors';
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+
+  margin-top: 1rem;
+`;
+
+export const LabelContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  margin-bottom: 2rem;
+
+  input {
+    height: 40px;
+    font-size: 18px;
+    border: 1px solid #ddd;
+    padding: 0 10px;
+    border-radius: 4px;
+    margin-top: 5px;
+
+    &:focus {
+      border: 1px solid ${colors.primaryColor};
+    }
+  }
 `;
