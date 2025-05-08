3682b55296e07b3826b0193a4085c37ae5b61538
Renan Nascimento <atomsfrontend@gmail.com>
2025-05-08 18:33:24 -0300

    feat: created component register with req axios and send data with form

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,10 +1,125 @@
-// Styled Component
+import React from 'react';
+
+// Axios
+import axios from '../../services/axios';
+
+// Lodash
+import get from 'lodash.get';
+
+// Styled Component Global
 import { Container } from '../../styles/GlobalStyles';
 
+// Styled Components
+import { Form, LabelContainer } from './styled';
+
+// React Router
+import { useNavigate } from 'react-router-dom';
+
+// Toastify
+import { toast } from 'react-toastify';
+
+// Validator
+import { isEmail } from 'validator';
+
 export default function Register() {
+  const navigate = useNavigate();
+  const [name, setName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    let formErrors = false;
+
+    if (!name) {
+      formErrors = true;
+      toast.warn('Campo nome não pode ser nulo.');
+    }
+
+    if (name.length < 3 || name.length > 100) {
+      formErrors = true;
+      toast.warn('Campo nome precisa ter entre 3 e 100 caracteres.');
+    }
+
+    if (!isEmail(email)) {
+      formErrors = true;
+      toast.warn('E-mail não é válido.');
+    }
+
+    if (pass.length < 1 || pass.length > 100) {
+      formErrors = true;
+      toast.warn('Campo senha precisa ter entre 12 e 100 caracteres.');
+    }
+
+    if (formErrors) return;
+
+    try {
+      const response = await axios.post('/users/create', {
+        name,
+        password,
+        email,
+      });
+
+      const { data, message, success } = response.data;
+
+      if (success) {
+        toast.success(`Usuário ${data.name} ${message}`);
+      }
+
+      navigate('/login');
+    } catch (e) {
+      const errors = get(e, 'response.data.data.erros', []);
+
+      errors && errors.map((err) => toast.error(err));
+    }
+  }
+
   return (
     <Container>
-      <h1>Register</h1>
+      <h1>Crie sua conta</h1>
+
+      <Form onSubmit={handleSubmit}>
+        <LabelContainer>
+          <label htmlFor="name">Name:</label>
+          <input
+            id="name"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Digite seu nome..."
+            aria-label="Campo para inserir o seu nome."
+            autoComplete="name"
+          />
+        </LabelContainer>
+        <LabelContainer>
+          <label htmlFor="email">E-mail:</label>
+          <input
+            id="email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Digite seu e-mail..."
+            aria-label="Campo para inserir o seu e-mail de login."
+            autoComplete="email"
+          />
+        </LabelContainer>
+        <LabelContainer>
+          <label htmlFor="pass">Senha:</label>
+          <input
+            id="pass"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Digite sua senha..."
+            aria-label="Campo para inserir a sua senha de login"
+            autoComplete="current-password"
+            minLength={12}
+          />
+        </LabelContainer>
+
+        <button type="submit">Registrar</button>
+      </Form>
     </Container>
   );
 }
