f0d694f6ef5d5268887c366e329dd3c81e504012
Renan Nascimento <atomsfrontend@gmail.com>
2025-05-10 17:37:27 -0300

    feat: created page photo students

--- src/pages/Photos/index.jsx
+++ src/pages/Photos/index.jsx
@@ -1,10 +1,108 @@
 // Styled Component
 import { Container } from '../../styles/GlobalStyles';
 
-export default function Photos() {
+// Styled
+import { Title, Form, LabelContainer } from './styled';
+
+// Axios
+import axios from '../../services/axios';
+
+// Redux
+import { useDispatch } from 'react-redux';
+import * as Actions from '../../store/modules/auth/actions';
+
+// React Router
+import { useNavigate } from 'react-router-dom';
+
+// Loadash
+import get from 'lodash.get';
+
+// Components
+import Loading from '../../components/Loading';
+
+// Toastify
+import { toast } from 'react-toastify';
+
+// Types
+import PropTypes from 'prop-types';
+
+export default function Photos({ match }) {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const id = get(match, 'params.id', '');
+
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [photo, setPhoto] = React.useState('');
+
+  React.useEffect(() => {
+    async function fetch() {
+      try {
+        setIsLoading(true);
+        const { data } = await axios.get(`/students/${id}`);
+        setPhoto(get(data, 'Photo[0].url', ''));
+        setIsLoading(false);
+      } catch (error) {
+        toast.error('Error a obter imagem.');
+        setIsLoading(false);
+        navigate('/');
+      }
+    }
+    fetch();
+  }, [id]);
+
+  async function handleChange(e) {
+    const photo = e.target.files[0];
+    const photoUrl = URL.createObjectURL(photo);
+
+    setPhoto(photoUrl);
+
+    const formData = new FormData();
+    formData.append('stundent_id', id);
+    formData.append('file', photo);
+
+    try {
+      setIsLoading(true);
+      await axios.post('/photo', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      toast.success('Foto enviada com sucesso.');
+      setIsLoading(false);
+    } catch (error) {
+      const status = get(error, 'response.data', '');
+      toast.error('Erro ao enviar arquivo');
+
+      if (status === 401) dispatch(Actions.ButtonLoginClickFailure());
+      setIsLoading(false);
+    }
+  }
+
   return (
     <Container>
-      <h1>Photos</h1>
+      <Loading isLoading={isLoading} />
+
+      <Title>Photos</Title>
+
+      <Form>
+        <LabelContainer>
+          <label htmlFor="photo">
+            {photo ? <img src={photo} alt="Foto" /> : 'Selecionar'}
+            <input
+              id="photo"
+              type="file"
+              onChange={handleChange}
+              placeholder="Digite seu nome..."
+              aria-label="Campo para inserir o seu arquivos."
+              autoComplete="name"
+            />
+          </label>
+        </LabelContainer>
+      </Form>
     </Container>
   );
 }
+Photos.propTypes = {
+  matche: PropTypes.shape({}).isRequired,
+};
---
+++ src/pages/Photos/styled.js
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+
+// Config Colors
+import * as colors from '../../config/colors';
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+
+  margin-top: 1rem;
+`;
+
+export const Title = styled.h1`
+  text-align: center;
+`;
+
+export const LabelContainer = styled.div`
+  label {
+    width: 180px;
+    height: 180px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #eee;
+    border: 5px dashed ${colors.primaryColor};
+    cursor: pointer;
+    border-radius: 50%;
+    overflow: hidden;
+  }
+
+  img {
+    width: 180px;
+    height: 180px;
+  }
+
+  input {
+    display: none;
+    height: 40px;
+    font-size: 18px;
+    border: 1px solid #ddd;
+    padding: 0 10px;
+    border-radius: 4px;
+    margin-top: 5px;
+
+    &:focus {
+      border: 1px solid ${colors.primaryColor};
+    }
+  }
+`;
